5c905bbed470085701a9cba0b1cce9f799c04fe2
Captain <aristetel@gmail.com>
2017-02-27 13:03:37 +0300

    fix

--- VolgaBer/views.py
+++ VolgaBer/views.py
@@ -24,7 +24,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('VolgaBer.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'VolgaBer/post_edit.html', {'form': form})
@@ -39,7 +39,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('VolgaBer.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'VolgaBer/post_edit.html', {'form': form})
